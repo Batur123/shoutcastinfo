aeff15984a077af970a32f75bbdeec1c3f5c000c
Batuhan Özkoç <batur.ozkoc@gmail.com>
2023-06-25 18:10:13 +0300

    Update to new version

--- shoutcastinfo.js
+++ shoutcastinfo.js
@@ -1,188 +1,159 @@
 const axios = require('axios');
 const xmljs = require('xml-js');
-const xmljsoptions = {ignoreComment: true, alwaysChildren: true, compact: true};
-const xmljsoptionswitharray = {ignoreComment: true, alwaysChildren: true, compact: true, trim: true, normalize: true, alwaysArray: true};
 
-/*
- * @param url
- * @param port
- * @param password
- * @param timeout
- * @param sid
- * @param op
- * @returns {PromiseLike<T> | Promise<T>}
- */
-function ShoutcastV2(ip,port,password,timeout,sid,op) {
-    var WholeURL = "";
-    const SHeaders = {timeout: timeout};
+const shoutcastV2 = async (ip, port, password, timeout, sid, op) => {
+  const getUrl = (page) => `http://${ip}:${port}/admin.cgi?pass=${password}&sid=${sid}&mode=viewxml&page=${page}`;
 
-    switch (op) {
-        //Experimental
-        case 'listeners': {
-            WholeURL = 'http://' + ip + ':' + port + '/admin.cgi?pass=' + password + '&sid=' + sid + '&mode=viewxml&page=3';
-            return axios.get(WholeURL, SHeaders)
-                .then(response => {
-                    let result = xmljs.xml2js(response.data, xmljsoptionswitharray);
+  switch (op) {
+    case 'listeners': {
+      try {
+        const response = await axios.get(getUrl(3), { timeout });
+        const result = xmljs.xml2js(response.data, {
+          ignoreComment: true,
+          alwaysChildren: true,
+          compact: true,
+          trim: true,
+          normalize: true,
+          alwaysArray: true
+        });
 
-                    return [
-                        typeof result?.SHOUTCASTSERVER[0]?.LISTENERS[0]?.LISTENER == 'undefined' ? "No Listener" : result?.SHOUTCASTSERVER[0]?.LISTENERS[0]?.LISTENER
-                    ];
-                })
-                .catch(function (error) {
-                    return error;
-                });
-        }
-
-        case 'streaminfo': {
-            WholeURL = 'http://' + ip + ':' + port + '/admin.cgi?pass=' + password + '&sid=' + sid + '&mode=viewxml&page=1';
-            return axios.get(WholeURL, SHeaders)
-                .then(response => {
-                    let result = xmljs.xml2js(response.data, xmljsoptions);
-                    return {
-                        CurrentListeners: typeof result?.SHOUTCASTSERVER?.CURRENTLISTENERS?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.CURRENTLISTENERS._text,
-                        PeakListeners: typeof result?.SHOUTCASTSERVER?.PEAKLISTENERS?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.PEAKLISTENERS._text,
-                        MaxListeners: typeof result?.SHOUTCASTSERVER?.MAXLISTENERS?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.MAXLISTENERS._text,
-                        UniqueListeners: typeof result?.SHOUTCASTSERVER?.UNIQUELISTENERS?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.UNIQUELISTENERS._text,
-                        AverageTime: typeof result?.SHOUTCASTSERVER?.AVERAGETIME?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.AVERAGETIME._text,
-                        StreamUptime: typeof result?.SHOUTCASTSERVER?.STREAMUPTIME?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.STREAMUPTIME._text,
-                        ServerGenre: typeof result?.SHOUTCASTSERVER?.SERVERGENRE?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERGENRE._text,
-                        ServerURL: typeof result?.SHOUTCASTSERVER?.SERVERURL?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERURL._text,
-                        ServerTitle: typeof result?.SHOUTCASTSERVER?.SERVERTITLE?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERTITLE._text,
-                        SongTitle: typeof result?.SHOUTCASTSERVER?.SONGTITLE?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SONGTITLE._text,
-                        StreamHits: typeof result?.SHOUTCASTSERVER?.STREAMHITS?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.STREAMHITS._text,
-                        StreamSource: typeof result?.SHOUTCASTSERVER?.STREAMSOURCE?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.STREAMSOURCE._text,
-                        Bitrate: typeof result?.SHOUTCASTSERVER?.BITRATE?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.BITRATE._text,
-                        Content: typeof result?.SHOUTCASTSERVER?.CONTENT?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.CONTENT._text,
-                        Version: typeof result?.SHOUTCASTSERVER?.VERSION?._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.VERSION._text,
-                    }
-                })
-                .catch(function (error) {
-                    return error;
-                });
-        }
-
-        default: {
-            throw new Error("Wrong operation.");
-        }
+        return [
+          result?.SHOUTCASTSERVER?.[0]?.LISTENERS?.[0]?.LISTENER ??
+            'No Listener'
+        ];
+      } catch (error) {
+        return error;
+      }
     }
-}
 
-/*
- * @param url
- * @param port
- * @param password
- * @param timeout
- * @param sid
- * @param op
- * @returns {PromiseLike<T> | Promise<T>}
- */
-function ShoutcastV1(ip,port,password,timeout,sid,op) {
-    var WholeURL = "";
-    const SHeaders = {
-        timeout: timeout,
-        headers: {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040614 Firefox/0.8'}
-    };
+    case 'streaminfo': {
+      try {
+        const response = await axios.get(getUrl(1), { timeout });
+        const result = xmljs.xml2js(response.data, {
+          ignoreComment: true,
+          alwaysChildren: true,
+          compact: true
+        });
 
-    switch (op) {
-        //Experimental
-        case 'listeners': {
-            WholeURL = 'http://' + ip + ':' + port + '/admin.cgi?pass=' + password + '&sid=' + sid + '&mode=viewxml&page=3';
-            return axios.get(WholeURL, SHeaders)
-                .then(response => {
-                    let result = xmljs.xml2js(response.data, xmljsoptionswitharray);
-
-                    return [
-                        typeof result.SHOUTCASTSERVER[0].LISTENERS[0].LISTENER == 'undefined' ? "No Listener" : result.SHOUTCASTSERVER[0].LISTENERS[0].LISTENER
-                    ];
-                })
-                .catch(function (error) {
-                    return error;
-                });
-        }
-
-        case 'streaminfo': {
-            WholeURL = 'http://' + ip + ':' + port + '/admin.cgi?pass=' + password + '&sid=' + sid + '&mode=viewxml&page=1';
-            return axios.get(WholeURL, SHeaders)
-                .then(response => {
-                    let result = xmljs.xml2js(response.data, xmljsoptions);
-                    console.log(result);
-                    return {
-                        CurrentListeners: typeof result.SHOUTCASTSERVER.CURRENTLISTENERS._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.CURRENTLISTENERS._text,
-                        PeakListeners: typeof result.SHOUTCASTSERVER.PEAKLISTENERS._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.PEAKLISTENERS._text,
-                        MaxListeners: typeof result.SHOUTCASTSERVER.MAXLISTENERS._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.MAXLISTENERS._text,
-                        UniqueListeners: typeof result.SHOUTCASTSERVER.PEAKLISTENERS._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.PEAKLISTENERS._text,
-                        AverageTime: typeof result.SHOUTCASTSERVER.AVERAGETIME._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.AVERAGETIME._text,
-                        StreamUptime: typeof result.SHOUTCASTSERVER.AVERAGETIME._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.AVERAGETIME._text,
-                        ServerGenre: typeof result.SHOUTCASTSERVER.SERVERGENRE._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERGENRE._text,
-                        ServerURL: typeof result.SHOUTCASTSERVER.SERVERURL._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERURL._text,
-                        ServerTitle: typeof result.SHOUTCASTSERVER.SERVERTITLE._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SERVERTITLE._text,
-                        SongTitle: typeof result.SHOUTCASTSERVER.SONGTITLE._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.SONGTITLE._text,
-                        StreamHits: typeof result.SHOUTCASTSERVER.STREAMHITS._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.STREAMHITS._text,
-                        StreamSource: 'Shoutcast v1 dont support StreamSource IP Address.',
-                        Bitrate: typeof result.SHOUTCASTSERVER.BITRATE._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.BITRATE._text,
-                        Content: typeof result.SHOUTCASTSERVER.CONTENT._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.CONTENT._text,
-                        Version: typeof result.SHOUTCASTSERVER.VERSION._text == 'undefined' ? "No Info" : result.SHOUTCASTSERVER.VERSION._text,
-                    }
-                })
-                .catch(function (error) {
-                    return error;
-                });
-        }
-
-        default: {
-            throw new Error("Wrong operation.");
-        }
+        return {
+          currentListeners:
+            result?.SHOUTCASTSERVER?.CURRENTLISTENERS?._text ?? 'No Info',
+          peakListeners: result?.SHOUTCASTSERVER?.PEAKLISTENERS?._text ?? 'No Info',
+          maxListeners: result?.SHOUTCASTSERVER?.MAXLISTENERS?._text ?? 'No Info',
+          uniqueListeners:
+            result?.SHOUTCASTSERVER?.UNIQUELISTENERS?._text ?? 'No Info',
+          averageTime: result?.SHOUTCASTSERVER?.AVERAGETIME?._text ?? 'No Info',
+          streamUptime: result?.SHOUTCASTSERVER?.STREAMUPTIME?._text ?? 'No Info',
+          serverGenre: result?.SHOUTCASTSERVER?.SERVERGENRE?._text ?? 'No Info',
+          serverURL: result?.SHOUTCASTSERVER?.SERVERURL?._text ?? 'No Info',
+          serverTitle: result?.SHOUTCASTSERVER?.SERVERTITLE?._text ?? 'No Info',
+          songTitle: result?.SHOUTCASTSERVER?.SONGTITLE?._text ?? 'No Info',
+          streamHits: result?.SHOUTCASTSERVER?.STREAMHITS?._text ?? 'No Info',
+          streamSource:
+            result?.SHOUTCASTSERVER?.STREAMSOURCE?._text ?? 'No Info',
+          bitrate: result?.SHOUTCASTSERVER?.BITRATE?._text ?? 'No Info',
+          content: result?.SHOUTCASTSERVER?.CONTENT?._text ?? 'No Info',
+          version: result?.SHOUTCASTSERVER?.VERSION?._text ?? 'No Info'
+        };
+      } catch (error) {
+        return error;
+      }
     }
-}
 
-
-/*
- * Song Info Main Function
- * @params args
- * @returns {*}
- */
-function shoutcastinfo(args) {
-    if (typeof args.ip == 'undefined' || !Boolean(args.ip)) throw new Error('IP is required');
-    if (typeof args.port == 'undefined' || !Boolean(args.port)) throw new Error('PORT is required');
-    if (typeof args.password == 'undefined' || !Boolean(args.password)) throw new Error('PASSWORD is required');
-    if (typeof args.type == 'undefined' || !Boolean(args.type)) throw new Error('Shoutcast type is wrong. Use 1 or 2');
-    if (typeof args.timeout == 'undefined' || !Boolean(args.timeout)) args.timeout = 2000;
-    if (typeof args.op == 'undefined' || !Boolean(args.op)) throw new Error('Operation is required.');
-
-    switch (args.type) {
-        case 1: {
-            //Experimental
-            return ShoutcastV1
-            (
-                args.ip,
-                args.port,
-                args.password,
-                args.timeout,
-                args.sid,
-                args.op
-            );
-        }
-        case 2: {
-            return ShoutcastV2
-            (
-                args.ip,
-                args.port,
-                args.password,
-                args.timeout,
-                args.sid,
-                args.op
-            );
-        }
-
-        default: {
-            throw new Error('Shoutcast type is wrong. Use 1 or 2');
-        }
+    default: {
+      throw new Error('Wrong operation.');
     }
-}
+  }
+};
 
+const shoutcastV1 = async (ip, port, password, timeout, sid, op) => {
+  const getUrl = (page) =>
+    `http://${ip}:${port}/admin.cgi?pass=${password}&sid=${sid}&mode=viewxml&page=${page}`;
 
-/**
- * Public method
- * @type {streamTitle}
- */
-module.exports = shoutcastinfo;
+  const headers = {
+    timeout,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.7) Gecko/20040614 Firefox/0.8',
+    },
+  };
+
+  switch (op) {
+    case 'listeners': {
+      try {
+        const response = await axios.get(getUrl(3), headers);
+        const result = xmljs.xml2js(response.data, {
+          ignoreComment: true,
+          alwaysChildren: true,
+          compact: true,
+          trim: true,
+          normalize: true,
+          alwaysArray: true,
+        });
+
+        return [
+          result?.SHOUTCASTSERVER?.[0]?.LISTENERS?.[0]?.LISTENER ??
+            'No Listener',
+        ];
+      } catch (error) {
+        return error;
+      }
+    }
+
+    case 'streaminfo': {
+      try {
+        const response = await axios.get(getUrl(1), headers);
+        const result = xmljs.xml2js(response.data, {
+          ignoreComment: true,
+          alwaysChildren: true,
+          compact: true,
+        });
+
+        return {
+          currentListeners:
+            result?.SHOUTCASTSERVER?.CURRENTLISTENERS?._text ?? 'No Info',
+          peakListeners: result?.SHOUTCASTSERVER?.PEAKLISTENERS?._text ?? 'No Info',
+          maxListeners: result?.SHOUTCASTSERVER?.MAXLISTENERS?._text ?? 'No Info',
+          uniqueListeners: result?.SHOUTCASTSERVER?.PEAKLISTENERS?._text ?? 'No Info',
+          averageTime: result?.SHOUTCASTSERVER?.AVERAGETIME?._text ?? 'No Info',
+          streamUptime: result?.SHOUTCASTSERVER?.AVERAGETIME?._text ?? 'No Info',
+          serverGenre: result?.SHOUTCASTSERVER?.SERVERGENRE?._text ?? 'No Info',
+          serverURL: result?.SHOUTCASTSERVER?.SERVERURL?._text ?? 'No Info',
+          serverTitle: result?.SHOUTCASTSERVER?.SERVERTITLE?._text ?? 'No Info',
+          songTitle: result?.SHOUTCASTSERVER?.SONGTITLE?._text ?? 'No Info',
+          streamHits: result?.SHOUTCASTSERVER?.STREAMHITS?._text ?? 'No Info',
+          streamSource: "Shoutcast v1 doesn't support StreamSource IP Address.",
+          bitrate: result?.SHOUTCASTSERVER?.BITRATE?._text ?? 'No Info',
+          content: result?.SHOUTCASTSERVER?.CONTENT?._text ?? 'No Info',
+          version: result?.SHOUTCASTSERVER?.VERSION?._text ?? 'No Info',
+        };
+      } catch (error) {
+        return error;
+      }
+    }
+
+    default: {
+      throw new Error('Wrong operation.');
+    }
+  }
+};
+
+const shoutcast = (args) => {
+  const {
+    ip,
+    port,
+    password,
+    type,
+    timeout = 2000,
+    op
+  } = args;
+
+  if (!ip) throw new Error('IP is required');
+  if (!port) throw new Error('PORT is required');
+  if (!password) throw new Error('PASSWORD is required');
+  if (typeof type !== 'number' || ![1, 2].includes(type)) throw new Error('Shoutcast type is wrong. Use 1 or 2');
+  if (!op) throw new Error('Operation is required.');
+
+  return (type === 1) ? shoutcastV1(ip, port, password, timeout, args.sid, op) : shoutcastV2(ip, port, password, timeout, args.sid, op);
+};
+
+module.exports = shoutcast;
